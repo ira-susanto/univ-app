cd1041ddde0fb0d931ce158c22b6388f6ad33e18
iRa <ira.susanto@racerocks3d.com>
2025-05-05 18:05:13 -0700

    Student show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,6 +21,10 @@ class StudentsController < ApplicationController
     end
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
   private
     # Only allow a list of trusted parameters through.
     def student_params

937a95d4198d2f7eeb09640d36fd37c087f03b7f
iRa <ira.susanto@racerocks3d.com>
2025-05-05 17:16:17 -0700

    Create a new student form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,11 @@
 class StudentsController < ApplicationController
-  
   # GET /students or /students.json
   def index
     @students = Student.all
   end
 
+  # GET /students/new
+  def new
+    @student = Student.new
+  end
 end
